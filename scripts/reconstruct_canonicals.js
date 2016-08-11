5338e58a133121fc1ca28ba51c8c8adc790257d7
Giovanni Campagna <gcampagn@cs.stanford.edu>
2016-08-10 17:57:54 -0700

    wip: examples

--- scripts/reconstruct_canonicals.js
+++ scripts/reconstruct_canonicals.js
@@ -10,6 +10,7 @@ require('thingengine-core/lib/polyfill');
 
 const Q = require('q');
 const fs = require('fs');
+const byline = require('byline');
 
 const ThingTalk = require('thingtalk');
 
@@ -20,10 +21,71 @@ const SchemaRetriever = ThingTalk.SchemaRetriever;
 
 var _schemaRetriever = new SchemaRetriever(new ThingPediaClient());
 
+const SPECIAL_TO_CANONICAL = {
+    hello: 'hello',
+    debug: 'debug',
+    help: 'help',
+    thankyou: 'thank you',
+    sorry: 'sorry',
+    cool: 'cool',
+    nevermind: 'never mind',
+    failed: 'failuretoparse'
+}
+
+function argToCanonical(buffer, arg) {
+    var match = /^tt[:\.]param\.(.+)$/.exec(arg.name.id);
+    if (match === null)
+        throw new TypeError('Argument name not in proper format, is ' + arg.name.id);
+    var argname = match[1];
+    var argcanonical = argname.replace(/_/g, ' ').replace(/([^A-Z])([A-Z])/g, '$1 $2').toLowerCase();
+    buffer.push(argcanonical);
+    buffer.push(arg.operator);
+
+    if (arg.type === 'Location') {
+        if (arg.relativeTag === 'rel_current_location')
+            buffer.push('here');
+        else if (arg.relativeTag === 'rel_home')
+            buffer.push('home');
+        else if (arg.relativeTag === 'rel_work')
+            buffer.push('work');
+        else if (arg.latitude === 37.442156 && arg.longitude === -122.1634471)
+            buffer.push('palo alto');
+        else if (arg.latitude === 34.0543942 && arg.longitude === -118.2439408)
+            buffer.push('los angeles');
+        else
+            buffer.push('some other place');
+    } else if (arg.type === 'String') {
+        buffer.push('"');
+        buffer.push(arg.value.value);
+        buffer.push('"');
+    } else {
+        buffer.push(String(arg.value.value));
+        if (arg.type === 'Measure')
+            buffer.push(arg.value.unit || arg.unit);
+    }
+}
+
 function reconstructCanonical(json) {
     var parsed = JSON.parse(json);
 
+    if (parsed.special)
+        return SPECIAL_TO_CANONICAL[parsed.special.id.substr('tt:root.special.'.length)];
+
     var buffer = [];
+    if (parsed.command) {
+        buffer.push(parsed.command.type);
+
+        if (parsed.command.value.value === 'generic')
+            return buffer.join(' ');
+
+        buffer.push(parsed.command.value.id.substr('tt:device.'.length));
+        return buffer.join(' ');
+    }
+    if (parsed.answer) {
+        argToCanonical(buffer, parsed.answer);
+        return buffer.join(' ');
+    }
+
     if (parsed.trigger)
         buffer.push('monitor if');
 
@@ -54,37 +116,8 @@ function reconstructCanonical(json) {
         buffer.push(meta.canonical);
 
         args.forEach(function(arg) {
-            buffer.push('with');
-            buffer.push('arg');
-
-            var match = /^tt[:\.]param\.(.+)$/.exec(arg.name.id);
-            if (match === null)
-                throw new TypeError('Argument name not in proper format, is ' + arg.name.id);
-            var argname = match[1];
-            var argcanonical = argname.replace(/_/g, ' ').replace(/([^A-Z])([A-Z])/g, '$1 $2').toLowerCase();
-            buffer.push(argcanonical);
-            buffer.push(arg.operator);
-
-            if (arg.type === 'Location') {
-                if (arg.relativeTag === 'rel_current_location')
-                    buffer.push('here');
-                else if (arg.relativeTag === 'rel_home')
-                    buffer.push('home');
-                else if (arg.relativeTag === 'rel_work')
-                    buffer.push('work');
-                else if (arg.latitude === 37.442156 && arg.longitude === -122.1634471)
-                    buffer.push('palo alto');
-                else
-                    buffer.push('los angeles');
-            } else if (arg.type === 'String') {
-                buffer.push('"');
-                buffer.push(arg.value.value);
-                buffer.push('"');
-            } else {
-                buffer.push(String(arg.value.value));
-                if (arg.type === 'Measure')
-                    buffer.push(arg.value.unit || arg.unit);
-            }
+            buffer.push('with arg');
+            argToCanonical(buffer, arg);
         });
 
         return buffer.join(' ');
@@ -93,6 +126,9 @@ function reconstructCanonical(json) {
 
 function main() {
     var output = fs.createWriteStream(process.argv[2]);
+    var onlineLearn = process.argv.length >= 4 ? byline(fs.createReadStream(process.argv[3])) : null;
+    if (onlineLearn !== null)
+        onlineLearn.setEncoding('utf8');
 
     db.withClient((dbClient) => {
         return exampleModel.getAll(dbClient);
@@ -113,7 +149,32 @@ function main() {
             });
         }));
     }).then(() => {
-        output.end();
+        if (onlineLearn === null) {
+            output.end();
+            return;
+        }
+
+        var promises = [];
+        onlineLearn.on('data', (data) => {
+            var line = data.split(/\t/);
+            var utterance = line[0];
+            var target_json = line[1];
+            promises.push(Q.try(function() {
+                return reconstructCanonical(target_json);
+            }).then(function(reconstructed) {
+                output.write(utterance);
+                output.write('\t');
+                output.write(reconstructed);
+                output.write('\n');
+            }).catch((e) => {
+                console.error('Failed to handle ' + utterance + ': ' + e.message);
+            }));
+        });
+        onlineLearn.on('end', () => {
+            Q.all(promises).then(() => {
+                output.end();
+            });
+        });
     }).done();
 
     output.on('finish', () => process.exit());
