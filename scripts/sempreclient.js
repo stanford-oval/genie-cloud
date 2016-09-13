8632ca6ae6095c96282140804a34cf02dffed789
Giovanni Campagna <gcampagn@cs.stanford.edu>
2016-09-12 18:20:47 -0700

    Add a tool to train SEMPRE with ThingTalk
    Add a command line tool that accepts commands and thingtalk and
    sends them to SEMPRE for learning/adding to the dataset.

---
+++ scripts/sempreclient.js
@@ -0,0 +1,107 @@
+// -*- mode: js; indent-tabs-mode: nil; js-basic-offset: 4 -*-
+//
+// This file is part of Sabrina
+//
+// Copyright 2016 Giovanni Campagna <gcampagn@cs.stanford.edu>
+//
+// See COPYING for details
+"use strict";
+
+const Q = require('q');
+const http = require('http');
+const https = require('https');
+const Url = require('url');
+
+const URL = 'https://pepperjack.stanford.edu';
+
+function getModule(parsed) {
+    if (parsed.protocol === 'https:')
+        return https;
+    else
+        return http;
+}
+
+function httpRequest(url) {
+    var parsed = Url.parse(url);
+    parsed.method = 'GET';
+
+    return Q.Promise(function(callback, errback) {
+        var req = getModule(parsed).request(parsed, function(res) {
+            if (res.statusCode == 302 ||
+                res.statusCode == 301 ||
+                res.statusCode == 303) {
+                res.resume();
+                httpRequest(res.headers['location']).then(callback, errback);
+                return;
+            }
+            if (res.statusCode >= 300) {
+                var data = '';
+                res.setEncoding('utf8');
+                res.on('data', function(chunk) {
+                    data += chunk;
+                });
+                res.on('end', function() {
+                    console.log('HTTP request failed: ' + data);
+                    errback(new Error('Unexpected HTTP error ' + res.statusCode));
+                });
+                return;
+            }
+
+            var data = '';
+            res.setEncoding('utf8');
+            res.on('data', function(chunk) {
+                data += chunk;
+            });
+            res.on('end', function() {
+                callback(data);
+            });
+        });
+        req.on('error', function(err) {
+            errback(err);
+        });
+        req.end();
+    });
+}
+
+module.exports = class SempreClient {
+    constructor(baseUrl, locale) {
+        this._baseUrl = baseUrl || URL;
+        this._locale = locale || 'en_US';
+        this._sessionId = undefined;
+
+        console.log('Using SEMPRE at ' + this._baseUrl + ' with locale ' + this._locale);
+    }
+
+    onlineLearn(utterance, json) {
+        var url = this._baseUrl + '/learn?locale=' + this._locale + '&q=' + encodeURIComponent(utterance)
+            + '&sessionId=' + this._sessionId + '&target=' + encodeURIComponent(json);
+        return httpRequest(url).then(() => {
+            console.log('Sent "' + utterance + '" to SEMPRE for learning');
+        }).catch((e) => {
+            console.error('Failed to send "' + utterance + '" to SEMPRE for learning: ' + e.message);
+        });
+    }
+
+    sendUtterance(utterance, expecting, choices) {
+        var url = this._baseUrl + '/query?locale=' + this._locale + '&limit=20&q=' + encodeURIComponent(utterance);
+        if (this._sessionId)
+            url += '&sessionId=' + this._sessionId;
+        if (expecting)
+            url += '&expect=' + encodeURIComponent(expecting);
+        if (choices) {
+            choices.forEach(function(c, i) {
+                if (c)
+                    url += '&choice[' + i + ']=' + encodeURIComponent(c);
+            });
+        }
+        return httpRequest(url).then((data) => {
+            var parsed = JSON.parse(data);
+            this._sessionId = parsed.sessionId;
+
+            if (parsed.error)
+                throw new Error('Error received from SEMPRE server: ' + parsed.error);
+
+            return parsed.candidates;
+        });
+    }
+}
---
+++ util/sempre_syntax.js
@@ -0,0 +1,206 @@
+// -*- mode: js; indent-tabs-mode: nil; js-basic-offset: 4 -*-
+//
+// This file is part of ThingEngine
+//
+// Copyright 2015-2016 Giovanni Campagna <gcampagn@cs.stanford.edu>
+//
+// See COPYING for details
+
+var Q = require('q');
+var TTGrammar = require('./reduced_grammar');
+
+function stringEscape(str) {
+    return '"' + str.replace(/([\"\'\\])/g, '\\$1').replace(/\n/g, '\\n') + '"';
+}
+// the following is to fix bugginess in GtkSourceView's syntax highlighting
+//[]/
+
+function codegenLocation(value) {
+    switch (value.relativeTag) {
+    case 'absolute':
+        return '$makeLocation(' + value.latitude + ', ' + value.longitude + ')';
+    case 'rel_home':
+        return '$home';
+    case 'rel_work':
+        return '$work';
+    case 'rel_current_location':
+        return '$here';
+    default:
+        throw new TypeError('Invalid relativeTag on location');
+    }
+}
+
+function codegenValue(type, value) {
+    switch (type) {
+    case 'Number':
+        return String(value.value);
+    case 'Measure':
+        return String(value.value) + value.unit;
+    case 'String':
+        return stringEscape(value.value);
+    case 'Date':
+        return '$makeDate(' + value.year + ', ' + value.month + ', ' + value.day + ')';
+    case 'Time':
+        return '$makeTime(' + value.hour + ', ' + value.minute + ')';
+    case 'Bool':
+        return String(value.value);
+    case 'EmailAddress':
+        return '$makeEmailAddress(' + stringEscape(value.value) + ')';
+    case 'PhoneNumber':
+        return '$makePhoneNumber(' + stringEscape(value.value) + ')';
+    case 'Location':
+        return codegenLocation(value);
+    case 'VarRef':
+        return value.id.substr('tt:param.'.length);
+    default:
+        throw new TypeError('Invalid value type ' + type);
+    }
+}
+
+function codegenArg(arg) {
+    if (arg.operator === 'has')
+        return '$contains(' + arg.name.id.substr('tt:param.'.length) + ', ' + codegenValue(arg.type, arg.value) + ')';
+    var op;
+    if (arg.operator === 'is')
+        op = '=';
+    else if (arg.operator == 'contains')
+        op = '=~';
+    else
+        op = arg.operator;
+
+    return arg.name.id.substr('tt:param.'.length) + ' ' + op + ' ' + codegenValue(arg.type, arg.value);
+}
+
+function codegenInvocation(invocation) {
+    var name = invocation.name.id;
+
+    return '@' + name.substr('tt:'.length) + '()'
+        + (invocation.args.map(function(a) { return ', ' + codegenArg(a); })).join('');
+}
+
+function codegenRule(rule) {
+    var buf = '';
+    if (!rule.trigger)
+        buf = '$now => ';
+    else
+        buf = codegenInvocation(rule.trigger) + ' => ';
+    if (rule.query)
+        buf += codegenInvocation(rule.query) + ' => ';
+    if (rule.action)
+        buf += codegenInvocation(rule.action);
+    else
+        buf = '@$notify()';
+    return buf;
+}
+
+function toThingTalk(sempre) {
+    if (sempre.rule)
+        return codegenRule(sempre.rule);
+    if (sempre.trigger)
+        return codegenInvocation(sempre.trigger) + ' => @$notify()';
+    if (sempre.query)
+        return '$now => ' + codegenInvocation(sempre.query) + ' => @$notify()';
+    if (sempre.action)
+        return '$now => ' + codegenInvocation(sempre.action);
+    throw new TypeError('Not rule, trigger, query or action');
+}
+
+function verifyOne(schemas, invocation, invocationType, scope) {
+    var match = /^tt:([a-z0-9A-Z_\-]+)\.([a-z0-9A-Z_]+)$/.exec(invocation.name.id);
+
+    return schemas.getMeta(match[1], invocationType, match[2]).then(function(schema) {
+        var argnames = {};
+        schema.args.forEach(function(name, i) {
+            argnames[name] = schema.schema[i];
+        });
+
+        invocation.args.forEach(function(arg) {
+            var argname = arg.name.id.substr('tt:param.'.length);
+            if (!(argname in argnames))
+                throw new TypeError('Invalid argument name ' + argname);
+            var type = argnames[argname];
+            var valuetype = type;
+
+            if (invocationType === 'actions' && arg.operator !== 'is')
+                throw new TypeError('Invalid operator ' + arg.operator + ' in argument to action');
+
+            switch (arg.operator) {
+            case 'is':
+                break;
+            case 'contains':
+                if (!type.isString)
+                    throw new TypeError('Left hand side of =~ must be string');
+                break;
+            case 'has':
+                if (!type.isArray)
+                    throw new TypeError('First argument of $contains must be array');
+                valuetype = type.elem;
+                break;
+            case '>':
+            case '<':
+                if (!type.isNumber && !type.isMeasure)
+                    throw new TypeError('Left hand side of ' + arg.operator + ' must be numeric');
+                break;
+            default:
+                throw new TypeError('Unknown operator ' + arg.operator);
+            }
+            if (arg.type === 'VarRef') {
+                var ref = arg.value.id.substr('tt:param.'.length);
+                if (!(ref in scope))
+                    throw new TypeError(ref + ' is not in scope');
+                if (!valuetype.equals(scope[ref]))
+                    throw new TypeError(ref + ' and ' + argname + ' are not type-compatible');
+            } else {
+                var valuehave = arg.type;
+                if (valuehave === valuetype.toString())
+                    return;
+                if (valuehave === 'Bool' && valuetype.isBoolean)
+                    return;
+                if (valuehave === 'Time' && valuetype.isString)
+                    return;
+                if (valuehave === 'Measure' && valuetype.isMeasure)
+                    return;
+                throw new TypeError('Invalid value type ' + valuehave + ', expected ' + valuetype);
+            }
+        });
+
+        // copy new variables in scope scope
+        for (var name in argnames)
+            scope[name] = argnames[name];
+
+        return scope;
+    });
+}
+
+function verify(schemas, prog) {
+    if (prog.rule) {
+        return Q.try(function() {
+            if (prog.rule.trigger)
+                return verifyOne(schemas, prog.rule.trigger, 'triggers', {});
+            else
+                return {};
+        }).then(function(scope) {
+            if (prog.rule.query)
+                return verifyOne(schemas, prog.rule.query, 'queries', scope);
+            else
+                return scope;
+        }).then(function(scope) {
+            if (prog.rule.action)
+                return verifyOne(schemas, prog.rule.action, 'actions', scope);
+            else
+                return scope;
+        });
+    } else if (prog.trigger) {
+        return verifyOne(schemas, prog.trigger, 'triggers', {});
+    } else if (prog.query) {
+        return verifyOne(schemas, prog.query, 'queries', {});
+    } else if (prog.action) {
+        return verifyOne(schemas, prog.action, 'actions', {});
+    }
+}
+
+module.exports = {
+    toSEMPRE: TTGrammar.parse,
+    toThingTalk: toThingTalk,
+    verify: verify
+}
