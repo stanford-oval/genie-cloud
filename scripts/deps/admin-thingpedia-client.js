8cc2ab1c4d2b3651d6b52c99890ac85bef6da418
Giovanni Campagna <gcampagn@cs.stanford.edu>
2017-12-07 11:35:30 -0800

    Replace second copy of SchemaRetriever with an AdminThingpediaClient
    Which does the same thing (give unlimited access to the Thingpedia
    database, given local DB access, ie a DATABASE_URL environment variable,
    with no developer key) with less code duplication. Especially
    because SchemaRetriever is somewhat in flux in ThingTalk at the
    moment.

---
+++ scripts/deps/admin-thingpedia-client.js
@@ -0,0 +1,59 @@
+// -*- mode: js; indent-tabs-mode: nil; js-basic-offset: 4 -*-
+//
+// This file is part of ThingEngine
+//
+// Copyright 2016 Giovanni Campagna <gcampagn@cs.stanford.edu>
+//
+// See COPYING for details
+"use strict";
+
+const db = require('../../util/db');
+const schema = require('../../model/schema');
+
+const BaseThingpediaClient = require('../../util/thingpedia-client');
+
+// A ThingpediaClient that always operates as admin, reading
+// the full database
+module.exports = class AdminThingpediaClient extends BaseThingpediaClient {
+    constructor(locale) {
+        super(null, locale);
+    }
+
+    getSchemas(schemas) {
+        return db.withClient((dbClient) =>
+            schema.getTypesAndNamesByKinds(dbClient, schemas, -1)
+        ).then((rows) => {
+            var obj = {};
+
+            rows.forEach((row) => {
+                obj[row.kind] = {
+                    kind_type: row.kind_type,
+                    triggers: row.triggers,
+                    actions: row.actions,
+                    queries: row.queries
+                };
+            });
+
+            return obj;
+        });
+    }
+
+    getMetas(schemas) {
+        return db.withClient((dbClient) =>
+            schema.getMetasByKinds(dbClient, schemas, -1, this.language)
+        ).then((rows) => {
+            var obj = {};
+
+            rows.forEach((row) => {
+                obj[row.kind] = {
+                    kind_type: row.kind_type,
+                    triggers: row.triggers,
+                    actions: row.actions,
+                    queries: row.queries
+                };
+            });
+
+            return obj;
+        });
+    }
+};
