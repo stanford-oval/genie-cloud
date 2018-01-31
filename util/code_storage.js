b01a4b603d9518907e2b7d76e1cdefbece82b5eb
Giovanni Campagna <gcampagn@cs.stanford.edu>
2018-01-31 09:21:14 -0800

    Update Copyright notices
    After rereading the University Copyright Agreement, it turns out
    that the university owns the copyright of this work (because it
    is sponsored research). This has been the case from the beginning,
    and previous copyright notices were factually wrong, so this is
    not a change or a relicensing.

--- util/code_storage.js
+++ util/code_storage.js
@@ -2,7 +2,9 @@
 //
 // This file is part of ThingEngine
 //
-// Copyright 2015 Giovanni Campagna <gcampagn@cs.stanford.edu>
+// Copyright 2015 The Board of Trustees of the Leland Stanford Junior University
+//
+// Author: Giovanni Campagna <gcampagn@cs.stanford.edu>
 //
 // See COPYING for details
 
