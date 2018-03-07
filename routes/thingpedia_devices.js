3b699001fe8ef77dd421b4a0024038fede0061d8
Giovanni Campagna <gcampagn@cs.stanford.edu>
2018-03-06 18:40:42 -0800

    More website work
    With a new thingpedia portal, replacing the old device list

--- routes/thingpedia_devices.js
+++ routes/thingpedia_devices.js
@@ -23,53 +23,8 @@ const exampleModel = require('../model/example');
 
 var router = express.Router();
 
-router.get('/', function(req, res) {
-    var page = req.query.page;
-    if (page === undefined)
-        page = 0;
-    page = parseInt(page);
-    if (isNaN(page) || page < 0)
-        page = 0;
-
-    db.withClient((client) => {
-        let devices;
-        if (req.user && req.user.developer_status >= user.DeveloperStatus.ADMIN)
-            devices = model.getAll(client, page * 9, 10);
-        else
-            devices = model.getAllApproved(client, page * 9, 10, req.user ? req.user.developer_org : null);
-        return devices.then((devices) => {
-            res.render('thingpedia_device_list', { page_title: req._("Thingpedia - Supported Devices"),
-                                                   page_h1: req._("Supported Devices"),
-                                                   csrfToken: req.csrfToken(),
-                                                   devices: devices,
-                                                   page_num: page });
-        });
-    }).done();
-});
-
-router.get('/search', function(req, res) {
-    var q = req.query.q;
-    if (!q) {
-        res.redirect('/thingpedia/devices');
-        return;
-    }
-
-    db.withTransaction(function(client) {
-        return model.getByFuzzySearch(client, q).then(function(devices) {
-            var kinds = new Set;
-            devices = devices.filter((d) => {
-                if (kinds.has(d.primary_kind))
-                    return false;
-                kinds.add(d.primary_kind);
-                return true;
-            });
-
-            res.render('thingpedia_device_list', { page_title: req._("Thingpedia - Supported Devices"),
-                                                   page_h1: req._("Results of Your Search"),
-                                                   csrfToken: req.csrfToken(),
-                                                   devices: devices });
-        });
-    }).done();
+router.get('/', (req, res) => {
+    res.redirect(301, '/thingpedia');
 });
 
 function localeToLanguage(locale) {
@@ -147,18 +102,7 @@ function getDetails(fn, param, req, res) {
     }).done();
 }
 
-const LEGACY_MAPS = {
-    'omlet': 'org.thingpedia.builtin.omlet',
-    'linkedin': 'com.linkedin',
-    'bodytrace-scale': 'com.bodytrace.scale',
-    'twitter-account': 'com.twitter',
-    'google-account': 'com.google',
-    'facebook': 'com.facebook',
-};
-
-router.get('/by-id/:kind', function(req, res) {
-    if (req.params.kind in LEGACY_MAPS)
-        req.params.kind = LEGACY_MAPS[req.params.kind];
+router.get('/by-id/:kind', (req, res) => {
     getDetails(model.getByPrimaryKind, req.params.kind, req, res);
 });
 
