9b5fb4e8fea1d26a4324c2721e4850ce596c94c0
Silei Xu <silei@cs.stanford.edu>
2018-03-14 21:19:58 -0700

    Update turking page - report detailed warnings for turkers to debug their paraphrase

--- public/javascripts/mturk_check.js
+++ public/javascripts/mturk_check.js
@@ -5,14 +5,9 @@
 
 $(document).ready(function() { 
     let checked = {};
-    let warnings = {};
-    for (let i = 1; i < 4; i ++) {
-        for (let j = 1; j < 3; j ++) {
+    for (let i = 1; i < 4; i ++) 
+        for (let j = 1; j < 3; j ++) 
             checked[`paraphrase${i}-${j}`] = false;
-            warnings[`paraphrase${i}-${j}`] = 'Something is wrong with this paraphrase!'
-        }
-    }
-
 
     $('.paraphrase').focusout(function() {
         let syntheticId = 'synthetic' + $(this).attr('id').substring('paraphrase'.length, 'paraphrasex'.length);
@@ -22,9 +17,9 @@ $(document).ready(function() {
         let warningId = 'warning' + paraphraseId.substring('paraphrase'.length);
         if (paraphrase.length > 0) {
             check(synthetic, paraphrase).then(function(res) {
-                checked[paraphraseId] = res; 
+                checked[paraphraseId] = res === 'passed'; 
                 console.log(checked);
-                if (res) {
+                if (res === 'passed') {
                     $('#' + warningId).prop('hidden', true);
                     if (allChecked(checked)) {
                         $('#submit').prop('disabled', false);
@@ -34,6 +29,7 @@ $(document).ready(function() {
                         $('#submit-warning').prop('hidden', false);
                     }
                 } else {
+                    $('#' + warningId).text(res);
                     $('#' + warningId).prop('hidden', false);
                     $('#submit').prop('disabled', true);
                     $('#submit-warning').prop('hidden', false);
@@ -72,28 +68,48 @@ function check(synthetic, paraphrase) {
         }),
     ).then(function() {
         console.log(entities_paraphrase, entities_synthetic)
-        return Object.keys(entities_synthetic).every(function(es) {
-            return Object.keys(entities_paraphrase).some(function(ep) {
+        for (let es in entities_synthetic) {
+            let found = false;
+            for (let ep in entities_paraphrase)
                 if (ep.substring(0, ep.length - 1) === es.substring(0, es.length - 1))
                     if (equal(entities_paraphrase[ep], entities_synthetic[es]))
-                        return true;
-                return false;
-            })
-        });
+                        found = true;
+            if (!found)
+                return `Cannot find ${value(entities_synthetic[es])} in your paraphrase.`
+        }
+        for (let ep in entities_paraphrase) {
+            let found = false;
+            for (let es in entities_synthetic)
+                if (ep.substring(0, ep.length - 1) === es.substring(0, es.length - 1))
+                    if (equal(entities_paraphrase[ep], entities_synthetic[es]))
+                        found = true;
+            if (!found)
+                return `Detect ${value(entities_paraphrase[ep])} in your paraphrase which not in the original sentence.`
+        }
+        return 'passed';
     });
 }
 
-function equal(value1, value2) {
-    if (typeof value1 !== typeof value2)
+function equal(entity1, entity2) {
+    if (typeof entity1 !== typeof entity2)
         return false;
-    let type = typeof value1;
+    let type = typeof entity1;
     if (type === 'string') 
-        return value1 === value2;
-    if ('latitude' in value1 && 'longitude' in value1) 
-        return value1.latitude === value2.latitude && value1.longitude === value2.longitude;
-    if ('value' in value1)
-        return value1.value === value2.value;
+        return entity1 === entity2;
+    if ('latitude' in entity1 && 'longitude' in entity1) 
+        return entity1.latitude === entity2.latitude && entity1.longitude === entity2.longitude;
+    if ('value' in entity1)
+        return entity1.value === entity2.value;
     return false;
+}
+
+function value(entity) {
+    if (typeof entity === "string")
+        return entity;
+    if ('value' in entity)
+        return entity.value;
+    if ('display' in entity)
+        return entity.display;
 }
 
 function allChecked(checked) {
