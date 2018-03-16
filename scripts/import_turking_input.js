be8942ec7a2ae5090f0a64909ba70228b29aeccc
Silei Xu <silei@cs.stanford.edu>
2018-03-16 13:35:03 -0700

    minor

--- scripts/import_turking_input.js
+++ scripts/import_turking_input.js
@@ -11,6 +11,7 @@
 
 const Q = require('q');
 const csv = require('csv');
+const fs = require('fs');
 const db = require('../util/db');
 
 function insert(dbClient, batchId, programs) {
@@ -30,6 +31,12 @@ function insert(dbClient, batchId, programs) {
         `insert into mturk_input(${columns.join(',')}) values (?)`, [row]);
 }
 
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
 
 function main() {
     const batch = process.argv[2];
@@ -46,21 +53,31 @@ function main() {
             parser.on('error', errback);
             parser.on('end', callback);
         }).then(() => {
+            shuffle(data);
             let programs = [];
-            let count = 0;
             return Q.all(data.map((row) => {
-                count ++;
                 let {id,code,sentence} = row;
                 programs.push({ id: parseInt(id), code: code, sentence: sentence });
-                if (count === 4) {
+                if (programs.length === 4) {
                     let tmp = programs;
                     programs = [];
-                    count = 0;
                     return insert(dbClient, batch, tmp);
                 } 
             }));
         });
-        
+    }).then(() => {
+        return db.withClient((dbClient) => {
+            const output = fs.createWriteStream(process.argv[3]);
+            const baseUrl = `https://almond.stanford.edu/mturk/${batch}/` 
+            let content = 'url\n';
+            return db.selectAll(dbClient, 'select id from mturk_input;', []).then((rows) => {
+                return rows.forEach((row) => {
+                    content += baseUrl + row.id + '\n';
+                })
+            }).then(() => {
+                return output.write(content);
+            });
+        });
     }).then(() => process.exit()).done();
 }
 
