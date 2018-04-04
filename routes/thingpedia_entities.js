764a3a363188aaf6e656bf948ad1dfe7724ebfe3
Giovanni Campagna <gcampagn@cs.stanford.edu>
2018-04-04 11:22:32 -0700

    Add the ability to create new entity types
    Given that now the system is a lot stricter about putting entity
    types in the database, we need a form for people to create new
    entity types

--- routes/thingpedia_entities.js
+++ routes/thingpedia_entities.js
@@ -14,6 +14,8 @@ const express = require('express');
 
 const db = require('../util/db');
 const model = require('../model/entity');
+const schemaModel = require('../model/schema');
+const user = require('../util/user');
 
 var router = express.Router();
 
@@ -22,6 +24,7 @@ router.get('/', (req, res) => {
         return model.getAll(dbClient);
     }).then((rows) => {
         res.render('thingpedia_entity_list', { page_title: req._("Thingpedia - Entity Types"),
+                                               csrfToken: req.csrfToken(),
                                                entities: rows });
     }).catch((e) => {
         res.status(500).render('error', { page_title: req._("Thingpedia - Error"),
@@ -38,6 +41,43 @@ router.get('/by-id/:id', (req, res) => {
                                                  values: values });
     }).catch((e) => {
         res.status(500).render('error', { page_title: req._("Thingpedia - Error"),
+                                          message: e });
+    }).done();
+});
+
+const NAME_REGEX = /([A-Za-z_][A-Za-z0-9_.-]*):([A-Za-z_][A-Za-z0-9_]*)/;
+
+router.post('/create', user.requireLogIn, user.requireDeveloper(), (req, res) => {
+    db.withTransaction((dbClient) => {
+        let match = NAME_REGEX.exec(req.body.entity_id);
+        if (match === null)
+            throw new Error('Invalid entity type ID');
+        if (!req.body.entity_name)
+            throw new Error('Invalid entity name');
+
+        let [, prefix, suffix] = match;
+
+        return Promise.resolve().then(() => {
+            if (req.user.developer_status < user.DeveloperStatus.ADMIN) {
+                return schemaModel.getByKind(dbClient, prefix).then((row) => {
+                    if (row.owner !== req.user.developer_org) throw new Error();
+                }).catch((e) => {
+                    console.log('err', e.message);
+                    throw new Error('The prefix of the entity ID must correspond to the ID of a Thingpedia device owned by your organization');
+                });
+            } else return Promise.resolve();
+        }).then(() => {
+            return model.create(dbClient, {
+                name: req.body.entity_name,
+                id: req.body.entity_id,
+                is_well_known: false,
+                has_ner_support: false
+            });
+        });
+    }).then(() => {
+        res.redirect(303, '/thingpedia/entities');
+    }).catch((e) => {
+        res.status(400).render('error', { page_title: req._("Thingpedia - Error"),
                                           message: e });
     }).done();
 });
