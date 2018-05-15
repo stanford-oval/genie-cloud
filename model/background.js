06bdc0a2dec35e64cfcf3c3f3665bf38191fb32f
Silei Xu <silei@cs.stanford.edu>
2018-05-15 09:17:26 -0700

    Add query part for brassau backgrounds

--- model/background.js
+++ model/background.js
@@ -26,8 +26,58 @@ function addRectangle(client, bg_id, rect) {
     ]);
 }
 
+function getRectangles(client, bg_id) {
+    return db.selectAll(client, "select * from background_rectangle where background_id = ?" [bg_id]);
+}
+
 function addTag(client, bg_id, tag) {
     return db.insertOne(client, "insert into background_tag values (?, ?, false)", [bg_id, tag]);
+}
+
+function getTags(client, bg_id) {
+    return db.selectAll(client, "select tag from background_tag where background_id = ?", [bg_id]);
+}
+
+function getBackground(client, bg_id) {
+    return db.selectOne(client, "select * from background where id = ? ", [bg_id]);
+}
+
+function processRectangleRows(rows) {
+    let out = {};
+    rows.forEach((row) => {
+        let rect = {
+            top: row.coord_top,
+            bottom: row.coord_bottom,
+            left: row.coord_left,
+            right: row.coord_right,
+            label: row.label,
+            index: row.order_index,
+            cover: row.cover,
+            'font-family': row.font_family,
+            'font-size': row.font_size,
+            'font-color': row.font_color,
+            'text_align': row.text_align,
+            color: JSON.parse(row.color),
+            'top-color': JSON.parse(row.top_color),
+            'bottom-color': JSON.parse(row.bottom_color),
+            'left-color': JSON.parse(row.left_color),
+            'right-color': JSON.parse(row.right_color),
+        };
+        if (row.background_id in out) {
+            out[row.background_id].rectangles.push(rect);
+        } else {
+            out[row.background_id] = {
+                owner: row.owner,
+                schema_id: row.schema_id,
+                function_name: row.function_name,
+                hash: row.hash,
+                'corner-colors': JSON.parse(row.corner_colors),
+                'color_palette': JSON.parse(row.color_palette),
+                rectangles: [rect]
+            };
+        }
+    });
+    return out;
 }
 
 module.exports = {
@@ -41,7 +91,10 @@ module.exports = {
         });
     },
 
-    get(client) {
-        throw new Error('Not implemented.');
+    getByTag(client, tag) {
+        return db.selectAll(client, "select bg.*, rect.* from background bg " +
+            "left join background_rectangle rect on bg.id = rect.background_id " +
+            "left join background_tag tag on bg.id = tag.background_id " +
+            "where tag.tag = ?", [tag]).then(processRectangleRows);
     }
 };
