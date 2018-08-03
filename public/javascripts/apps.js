ee9c9c7608bab217344228804d3e4e77d668b6fa
Silei Xu <silei@cs.stanford.edu>
2018-08-03 09:57:24 -0700

    Merge My Almond and Web Almond

--- public/javascripts/apps.js
+++ public/javascripts/apps.js
@@ -2,4 +2,17 @@ $(function() {
     $('.form-delete').on('submit', function() {
         return confirm("Are you sure?");
     });
+
+    let coll = $('.collapsible');
+    for (let i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            let content = this.nextElementSibling;
+            if (content.style.display === "block") {
+                content.style.display = "none";
+            } else {
+                content.style.display = "block";
+            }
+        });
+    }
 });
