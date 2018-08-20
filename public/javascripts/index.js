49e468b68dea5975c180114f08ec2d9cf5fea0f0
Silei Xu <silei@cs.stanford.edu>
2018-08-20 10:06:12 -0700

    Update subscribe interface - hide the input box until button is clicked - show on both app page & research page (move the js code to shared.js)

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,12 +1,3 @@
 (function() {
     ThingEngine.setCloudIdWhenReady();
 })();
-
-$(function() {
-    $('#subscribe-submit').click(function(event) {
-        $.post('/user/subscribe', {'_csrf': $(this).attr('csrf'), 'email': $('#subscribe-email').val() } );
-        $('#subscribe-submit').hide();
-        $('#subscribe-done').show();
-        event.preventDefault();
-    });
-});
