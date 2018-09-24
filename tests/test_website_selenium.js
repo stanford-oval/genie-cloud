af39e58680daf1c5b72c2e0d3ffd1f1623203bc4
Giovanni Campagna <gcampagn@cs.stanford.edu>
2018-09-23 22:23:46 -0700

    Run selenium tests in headless mode
    Travis does not like regular mode (it requires Xfvb, which is
    annoying to set up), and it doesn't buy us much.
    
    Also, run an actual test against the content of the page, now
    that we cannot see what it says.

--- tests/test_website_selenium.js
+++ tests/test_website_selenium.js
@@ -13,17 +13,31 @@ require('thingengine-core/lib/polyfill');
 require('./polyfill');
 process.on('unhandledRejection', (up) => { throw up; });
 
-//const assert = require('assert');
+const assert = require('assert');
 
 const webdriver = require('selenium-webdriver');
-//const chrome = require('selenium-webdriver/chrome');
-//const firefox = require('selenium-webdriver/firefox');
+const chrome = require('selenium-webdriver/chrome');
+const firefox = require('selenium-webdriver/firefox');
 
 const BASE_URL = process.env.THINGENGINE_URL || 'http://127.0.0.1:8080';
 
 async function withSelenium(test) {
-    const driver = new webdriver.Builder().
-        forBrowser('firefox').build();
+    const builder = new webdriver.Builder()
+        .forBrowser('firefox');
+
+    // on Travis CI we run headless; setting up Xvfb is
+    // just annoying and not very useful
+    if (process.env.TRAVIS) {
+        builder
+        .setFirefoxOptions(
+            new firefox.Options().headless()
+        )
+        .setChromeOptions(
+            new chrome.Options().headless()
+        );
+    }
+
+    const driver = builder.build();
     try {
         await test(driver);
     } finally {
@@ -33,7 +47,12 @@ async function withSelenium(test) {
 
 async function testBasic(driver) {
     await driver.get(BASE_URL + '/');
-    await driver.sleep(30000);
+
+    const title = await driver.wait(
+        webdriver.until.elementLocated(webdriver.By.id('almond-title')),
+        30000);
+
+    assert.strictEqual(await title.getText(), 'Almond');
 }
 
 async function main() {
