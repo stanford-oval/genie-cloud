49cb2fbc41e31721eb078e70404c8297b4723d8d
Silei Xu <silei@cs.stanford.edu>
2018-10-25 18:23:06 -0700

    Fix lint

--- routes/thingpedia_schemas.js
+++ routes/thingpedia_schemas.js
@@ -61,7 +61,7 @@ router.get('/by-id/:kind', (req, res, next) => {
         const code = classDef.prettyprint();
         const highlightedCode = highlightjs.highlight('tt', code).value;
         const dataset = DatasetUtils.examplesToDataset(req.params.kind, 'en', examples,
-            { editMode: true })
+            { editMode: true });
         const highlighedDataset = highlightjs.highlight('tt', dataset).value;
 
         const row = {
