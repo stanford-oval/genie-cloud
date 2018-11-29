829d944023410bfd44d14a1f0cd8c320a3a79710
Giovanni Campagna <gcampagn@cs.stanford.edu>
2018-11-29 11:13:42 -0800

    Replace hacked build scripts with single Makefile
    Simplifies updates, and avoids rebuilding everything for any change

--- scripts/update-bundles.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-set -x
-
-for i in trainer commandpedia thingpedia-device-create thingpedia-portal ; do
-	browserify -o public/javascripts/${i}-bundle.js browser/${i}.js
-done
