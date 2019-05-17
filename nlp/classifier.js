1141bd6d4ff5b935544fed9889df65ff472168bc
Your Name <ryachen@nuevaschool.org>
2019-05-16 21:30:23 -0700

    adding python scripts

---
+++ nlp/classifier.js
@@ -0,0 +1,33 @@
+const spawn = require('child_process').spawn;
+
+class NLPClassifier {
+
+	 constructor(){
+
+		this.pythonProcess = spawn('python3',['-u', "python_classifier/classifier.py"]);
+
+		this.pythonProcess.stderr.on('data', (data) => {
+
+		  console.log(data.toString())
+		});
+	}
+
+	async classify(input){
+
+		let process = this.pythonProcess
+		var promise = new Promise(function(resolve, reject) {
+			process.stdout.on('data', async (data)  =>  {
+
+				var receiveData = await data.toString()
+				resolve (receiveData)
+			});
+		});
+
+		process.stdin.write(input);
+		process.stdin.end();
+
+		return promise
+
+	}
+
+}
---
+++ nlp/python_classifier/classifier.py
@@ -0,0 +1,74 @@
+from keras.models import load_model
+from bert_embedding import BertEmbedding
+#Import BERT Embedding from PyPi
+
+import numpy, math, sys
+
+bert_embedding = BertEmbedding()
+
+model = load_model('python_classifier/classifier.h5')
+
+
+def encode_sentence(sentence):
+
+    #Takes input of list of sentences and returns numpy array of length 20 with the BERT Embedding
+
+    encoded_sentence = numpy.zeros(15360)
+
+
+    result = bert_embedding([sentence])
+    index = 0
+    for j in range(len(result[0][1])):
+        if j < 20:
+            for k in range(len(result[0][1][j])):
+
+                encoded_sentence[index] = result[0][1][j][k]
+                index += 1
+
+
+    return encoded_sentence
+
+def output_prediction(sentence):
+
+    if sentence != None:
+
+        encoded = encode_sentence(sentence)
+
+        test_data = numpy.zeros([1, 15360])
+        test_data[0] = encoded
+
+
+        percentages = model.predict(test_data)
+
+        total = 0
+
+        adjusted_percentages = []
+        for i in range(4):
+
+            adjustedValue = math.sqrt(percentages[0][i])
+            adjusted_percentages.append(adjustedValue)
+            total = total + adjustedValue
+
+
+        final_percentages = []
+        for j in range(4):
+            final_percentages.append(adjusted_percentages[j]/total)
+
+
+        return final_percentages
+    else:
+        return (0.25,0.25,0.25,0.25)
+
+def main():
+
+    #print probabilities based on user input
+    probabilities = (output_prediction(sys.stdin.readlines()[0]))
+    print(probabilities[0])
+    print(probabilities[1])
+    print(probabilities[2])
+    print(probabilities[3])
+
+
+
+if __name__ == '__main__':
+    main()
