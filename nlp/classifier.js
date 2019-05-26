21a60d1da6ee17d326313623736ccd9253d1d722
Giovanni Campagna <gcampagn@cs.stanford.edu>
2019-05-26 10:57:17 -0700

    Fix code style in nlp/classifier
    4 spaces, no tabs.
    Also add copyright comment.

--- nlp/classifier.js
+++ nlp/classifier.js
@@ -1,75 +1,82 @@
+// -*- mode: js; indent-tabs-mode: nil; js-basic-offset: 4 -*-
+//
+// This file is part of Almond
+//
+// Copyright 2019 Ryan Cheng <ryachen@nuevaschool.org>
+//
+// See COPYING for details
 "use strict";
 
-const spawn = require('child_process').spawn;
+const child_process = require('child_process');
+
 const JsonDatagramSocket = require('../util/json_datagram_socket');
 
-class NLPClassifier{
+class NLPClassifier {
+    constructor() {
+        // spawn python process
+        this.pythonProcess = child_process.spawn('python3', ['-u',
+            __dirname + "/python_classifier/classifier.py"]);
 
-	constructor() {
-		//spawn python process
-		this.pythonProcess = spawn('python3',['-u', __dirname + "/python_classifier/classifier.py"]);
+        this.concurrentRequests = new Map();
+        this.isLive = true;
+        this.counter = 0;
 
-		this.concurrentRequests = new Map();
-		this.isLive = true;
-		this.counter = 0;
+        this._stream = new JsonDatagramSocket(this.pythonProcess.stdout, this.pythonProcess.stdin, 'utf8');
+        this._stream.on('data', (msg) => {
+            const id = msg.id;
 
-		this._stream = new JsonDatagramSocket(this.pythonProcess.stdout, this.pythonProcess.stdin, 'utf8');
-		this._stream.on('data', (msg) => {
+            // matches id of request to handle concurrent requests
+            if (msg.error) {
+                this.concurrentRequests.get(id).reject(msg);
+                this.concurrentRequests.delete(id);
+            } else {
+                this.concurrentRequests.get(id).resolve(msg);
+                this.concurrentRequests.delete(id);
+            }
+        });
 
-			const id = parseInt(msg.id);
-			//matches id of request to handle concurrent requests
-			if (msg.error) {
-				this.concurrentRequests.get(id).reject(msg);
-				this.concurrentRequests.delete(id);
-			}else {
-				this.concurrentRequests.get(id).resolve(msg);
-				this.concurrentRequests.delete(id);
-			}
+        // error handling
+        this._stream.on('error', (msg) => {
+            console.log("error occured");
+        });
+        this._stream.on('end', (error) => {
+            console.log("Ending Python Process");
+        });
+        this._stream.on('close', (hadError) => {
+            console.log("Closing Python Process");
+            this.isLive = false;
+            for (let { reject } of this.concurrentRequests.values())
+                reject(new Error("Python Process Closed"));
+        });
+    }
 
-		});
+    _newPromise(id){
+        var new_promise = {
+            promise: null,
+            resolve: null,
+            reject: null,
+            uniqueid: id
+        };
+        new_promise.promise = new Promise((resolve, reject) => {
+            new_promise.resolve = resolve;
+            new_promise.reject = reject;
+        });
+        return new_promise;
+    }
 
-		//error handling
-		this._stream.on('error', (msg) => {
-			console.log("error occured");
-		});
-		this._stream.on('end', (error) => {
-			console.log("Ending Python Process");
-		});
-		this._stream.on('close', (hadError) => {
-			console.log("Closing Python Process");
-			this.isLive = false;
-			for (let { reject } of this.concurrentRequests.values())
-			reject(Error("Python Process Closed"));
-		});
-	}
-
-	newPromise(id){
-		var new_promise = {
-			promise: null,
-			resolve: null,
-			reject: null,
-			uniqueid: id
-		};
-		new_promise.promise = new Promise((resolve, reject) => {
-			new_promise.resolve = resolve;
-			new_promise.reject = reject;
-		});
-		return new_promise;
-	}
-
-	classify(input){
-		const new_promise = this.newPromise(this.counter);
-		this.concurrentRequests.set(this.counter, new_promise);
-		if (!this.isLive){
-			new_promise.reject(Error("Python Process Dead"));
-		}else{
-			this._stream.write(
-				{id: this.counter, sentence: input }
-			);
-			this.counter += 1;
-		}
-		return new_promise.promise;
-	}
+    classify(input){
+        const new_promise = this._newPromise(this.counter);
+        if (!this.isLive) {
+            new_promise.reject(Error("Python Process Dead"));
+        } else {
+            this._stream.write(
+                { id: this.counter, sentence: input }
+            );
+            this.concurrentRequests.set(this.counter, new_promise);
+            this.counter += 1;
+        }
+        return new_promise.promise;
+    }
 }
 
 module.exports = new NLPClassifier();
