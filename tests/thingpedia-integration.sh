24710964ce46edfc3524b98a1102f2cbfc05d670
Giovanni Campagna <gcampagn@cs.stanford.edu>
2019-07-30 16:39:13 -0700

    Update reference sha256sum
    And make sure we download the dataset in a predictable order, regardless
    of what index mysql chooses to use

--- tests/thingpedia-integration.sh
+++ tests/thingpedia-integration.sh
@@ -177,7 +177,7 @@ mkdir jobdir
 
 sha256sum exact.tsv jobdir/eval.tsv jobdir/train.tsv
 sha256sum -c <<EOF
-8d73044c4360393a00436b90b48d9b31a338985d93341feb17d202af675c33c6  exact.tsv
+e4ebd87581449018bf57056f7a844d118d67ee8d04554c5e72934ba02226120d  exact.tsv
 72bccb2f8b7d4c6b6556eb5c998374c0b10b1e78c56f3ba7f72ea08cd5a8f240  jobdir/eval.tsv
 ef125403ed0ba1e0b8af7191a16d3ad2d12726f1c6a7f3f1b06e1d1add3af6c3  jobdir/train.tsv
 EOF
