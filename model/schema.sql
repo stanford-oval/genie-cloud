fa7aa77ee3f3cb12011472d09c580934abbb72a7
Giovanni Campagna <gcampagn@cs.stanford.edu>
2019-07-30 11:21:30 -0700

    Add database schema for Alexa models
    Which will target different subset of Thingpedia, much like the LUInet
    models.

--- model/schema.sql
+++ model/schema.sql
@@ -684,7 +684,8 @@ CREATE TABLE `model_devices` (
   `schema_id` int(11) NOT NULL,
   PRIMARY KEY (`model_id`, `schema_id`),
   KEY `schema_id` (`schema_id`),
-  CONSTRAINT `model_devices_ibfk_1` FOREIGN KEY (`schema_id`) REFERENCES `device_schema` (`id`) ON UPDATE CASCADE ON DELETE RESTRICT
+  CONSTRAINT `model_devices_ibfk_1` FOREIGN KEY (`schema_id`) REFERENCES `device_schema` (`id`) ON UPDATE CASCADE ON DELETE RESTRICT,
+  CONSTRAINT `model_devices_ibfk_2` FOREIGN KEY (`model_id`) REFERENCES `models` (`id`) ON UPDATE CASCADE ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -794,6 +795,46 @@ CREATE TABLE `homepage_links` (
   KEY `upd_date` (`upd_date`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `alexa_models`
+--
+
+DROP TABLE IF EXISTS `alexa_models`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `alexa_models` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `language` char(15) COLLATE utf8_bin NOT NULL DEFAULT 'en',
+  `tag` varchar(64) COLLATE utf8_bin,
+  `owner` int(11) NOT NULL,
+  `access_token` char(64) COLLATE utf8_bin NULL,
+  `anonymous_user` int(11) NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY (`language`, `tag`),
+  KEY `owner` (`owner`),
+  CONSTRAINT `alexa_models_ibfk_1` FOREIGN KEY (`owner`) REFERENCES `organizations` (`id`) ON UPDATE CASCADE ON DELETE CASCADE,
+  CONSTRAINT `alexa_models_ibfk_2` FOREIGN KEY (`anonymous_user`) REFERENCES `users` (`id`) ON UPDATE CASCADE ON DELETE RESTRICT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `alexa_model_devices`
+--
+
+DROP TABLE IF EXISTS `alexa_model_devices`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `alexa_model_devices` (
+  `model_id` int(11) NOT NULL,
+  `schema_id` int(11) NOT NULL,
+  PRIMARY KEY (`model_id`, `schema_id`),
+  KEY `schema_id` (`schema_id`),
+  CONSTRAINT `alexa_model_devices_ibfk_1` FOREIGN KEY (`schema_id`) REFERENCES `device_schema` (`id`) ON UPDATE CASCADE ON DELETE RESTRICT,
+  CONSTRAINT `alexa_model_devices_ibfk_2` FOREIGN KEY (`model_id`) REFERENCES `alexa_models` (`id`) ON UPDATE CASCADE ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
 
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
