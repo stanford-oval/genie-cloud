a99b2237dd9e1e633d8e4d653a6a16d64c429a97
Giovanni Campagna <gcampagn@cs.stanford.edu>
2019-09-13 00:33:33 +0200

    Remove duplicate code that is now exposed by Genie
    The copy in Genie is tested, we don't need another buggy copy here

--- routes/mturk.js
+++ routes/mturk.js
@@ -15,11 +15,12 @@ const csvparse = require('csv-parse');
 const csvstringify = require('csv-stringify');
 const express = require('express');
 const db = require('../util/db');
-const ThingTalk = require('thingtalk');
 const multer = require('multer');
 const csurf = require('csurf');
 const Stream = require('stream');
 const seedrandom = require('seedrandom');
+const ThingTalk = require('thingtalk');
+const Genie = require('genie-toolkit');
 
 const user = require('../util/user');
 const platform = require('../util/platform');
@@ -29,7 +30,6 @@ const AdminThingpediaClient = require('../util/admin-thingpedia-client');
 const TokenizerService = require('../util/tokenizer_service');
 const iv = require('../util/input_validation');
 const { BadRequestError, ForbiddenError, InternalError, NotFoundError } = require('../util/errors');
-const { shuffle } = require('../util/random');
 
 const Config = require('../config');
 
@@ -164,112 +164,6 @@ router.get('/validation/csv/:batch', user.requireLogIn, user.requireRole(user.Ro
     }).catch(next);
 });
 
-
-// FIXME expose the copy in Genie instead
-
-
-function quickGetFunctions(code) {
-    const devices = [];
-    const functions = [];
-
-    const regex = /@([a-z0-9_.]+)([a-z0-9_]+)\(/g;
-
-    let match = regex.exec(code);
-    while (match !== null) {
-        devices.push(match[1]);
-        functions.push(match[2]);
-        match = regex.exec(code);
-    }
-    return [devices, functions];
-}
-
-function subset(array1, array2) {
-    for (let el of array1) {
-        if (array2.indexOf(el) < 0)
-            return false;
-    }
-    return true;
-}
-
-// generate a fake parphrase with same device(s) but different functions
-function fakeParaphrase(batch, targetCode) {
-    const [devices, functions] = quickGetFunctions(targetCode);
-
-    for (let candidate of batch) {
-        const [candDevices, candFunctions] = quickGetFunctions(candidate.target_code);
-
-        if (subset(devices, candDevices) && !subset(functions, candFunctions))
-            return candidate.paraphrase;
-    }
-
-    // return something
-    return 'if reddit front page updated, get a #dog gif';
-}
-
-class ValidationHITCreator extends Stream.Transform {
-    constructor(batch, options) {
-        super({
-            readableObjectMode: true,
-            writableObjectMode: true,
-        });
-
-        this._batch = batch;
-
-        this._i = 0;
-        this._buffer = {};
-
-        this._debug = options.debug;
-        this._targetSize = options.targetSize;
-        this._sentencesPerTask = options.sentencesPerTask;
-        this._rng = options.rng;
-    }
-
-    _transform(row, encoding, callback) {
-        if (row.paraphrases.length < this._targetSize) {
-            if (this._debug)
-                console.log(`Skipped synthetic sentence ${row.synthetic_id}: not enough paraphrases`);
-            callback();
-            return;
-        }
-
-        const i = ++this._i;
-        this._buffer[`id${i}`] = row.synthetic_id;
-        this._buffer[`thingtalk${i}`] = row.target_code;
-        this._buffer[`sentence${i}`] = row.synthetic;
-
-        const fakeSame = row.synthetic;
-        const fakeDifferent = fakeParaphrase(this._batch, row.target_code);
-        const paraphrases = [{
-            id: '-same',
-            paraphrase: fakeSame
-        }, {
-            id: '-different',
-            paraphrase: fakeDifferent,
-        }].concat(row.paraphrases);
-
-        shuffle(paraphrases, this._rng);
-        this._buffer[`index_same${i}`] = 1 + paraphrases.findIndex((el) => el.id === '-same');
-        this._buffer[`index_diff${i}`] = 1 + paraphrases.findIndex((el) => el.id === '-different');
-
-        for (let j = 0; j < paraphrases.length; j++) {
-            let {id, paraphrase} = paraphrases[j];
-            this._buffer[`id${i}-${j+1}`] = id;
-            this._buffer[`paraphrase${i}-${j+1}`] = paraphrase;
-        }
-
-        if (i === this._sentencesPerTask) {
-            this.push(this._buffer);
-            this._i = 0;
-            this._buffer = {};
-        }
-        callback();
-    }
-
-    _flush(callback) {
-        process.nextTick(callback);
-    }
-}
-
 class ValidationHITInserter extends Stream.Writable {
     constructor(dbClient, batch, targetSize) {
         super({ objectMode: true });
@@ -377,7 +271,7 @@ router.post('/start-validation', user.requireLogIn, user.requireRole(user.Role.N
                 }
             };
 
-            const creator = new ValidationHITCreator(allSynthetics, {
+            const creator = new Genie.ValidationHITCreator(allSynthetics, {
                 targetSize,
                 sentencesPerTask: SYNTHETIC_PER_PARAPHRASE_HIT,
                 rng: seedrandom.alea('almond is awesome')
