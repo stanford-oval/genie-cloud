e31de39f06e4d083e334f2a7abb7bbbafb18d2a8
Giovanni Campagna <gcampagn@cs.stanford.edu>
2019-09-26 11:11:54 -0700

    tests: fix staging NLP url
    It moved, and the old URL is not always active or working

--- tests/thingpedia-integration.sh
+++ tests/thingpedia-integration.sh
@@ -27,7 +27,7 @@ module.exports.DISCOURSE_SSO_SECRET = 'd836444a9e4084d5b224a60c208dce14';
 module.exports.AES_SECRET_KEY = '80bb23f93126074ba01410c8a2278c0c';
 module.exports.JWT_SIGNING_KEY = "not so secret key" ;
 module.exports.SECRET_KEY = "not so secret key";
-module.exports.NL_SERVER_URL = "https://almond-dev.stanford.edu/nnparser";
+module.exports.NL_SERVER_URL = "https://nlp-staging.almond.stanford.edu";
 module.exports.SUPPORTED_LANGUAGES = ['en-US', 'it-IT', 'zh-CN', 'zh-TW'];
 EOF
 
@@ -110,7 +110,7 @@ module.exports.DISCOURSE_SSO_SECRET = 'd836444a9e4084d5b224a60c208dce14';
 module.exports.AES_SECRET_KEY = '80bb23f93126074ba01410c8a2278c0c';
 module.exports.JWT_SIGNING_KEY = "not so secret key" ;
 module.exports.SECRET_KEY = "not so secret key";
-module.exports.NL_SERVER_URL = "https://almond-dev.stanford.edu/nnparser";
+module.exports.NL_SERVER_URL = "https://nlp-staging.almond.stanford.edu";
 module.exports.SUPPORTED_LANGUAGES = ['en-US', 'it-IT', 'zh-CN', 'zh-TW'];
 EOF
 
