1252155868bbb0b762b4f91d045b1bc387fb81c9
Giovanni Campagna <gcampagn@cs.stanford.edu>
2019-10-21 21:41:09 -0700

    training: add artificial delay of one minute before touching S3
    This ensures that kube2iam is properly started even on freshly
    created nodes, and we get the correct credentials.

---
+++ util/sleep.js
@@ -0,0 +1,16 @@
+// -*- mode: js; indent-tabs-mode: nil; js-basic-offset: 4 -*-
+//
+// This file is part of ThingEngine
+//
+// Copyright 2019 The Board of Trustees of the Leland Stanford Junior University
+//
+// Author: Giovanni Campagna <gcampagn@cs.stanford.edu>
+//
+// See COPYING for details
+"use strict";
+
+module.exports = async function sleep(ms) {
+    return new Promise((resolve, reject) => {
+        setTimeout(resolve, ms);
+    });
+};
