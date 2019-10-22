159c7f676395077e907be738a7222bfc43779dd5
jgd5 <root@ip-172-31-35-232.us-west-2.compute.internal>
2019-10-22 02:50:25 +0000

    fix k8sApi.deleteNamespacedJob due to api change

--- tests/unit/index.js
+++ tests/unit/index.js
@@ -26,5 +26,6 @@ seq([
     ('./test_exact_matcher'),
     ('./test_nlp_compat'),
     ('./test_example_names'),
-    ('./test_alexa_intent_parser')
+    ('./test_alexa_intent_parser'),
+    ('./test_k8s_api.js')
 ]);
---
+++ tests/unit/test_k8s_api.js
@@ -0,0 +1,57 @@
+// -*- mode: js; indent-tabs-mode: nil; js-basic-offset: 4 -*-
+//
+// This file is part of ThingEngine
+//
+// Copyright 2019 The Board of Trustees of the Leland Stanford Junior University
+//
+// Author: Giovanni Campagna <gcampagn@cs.stanford.edu>
+//
+// See COPYING for details
+'use strict';
+
+function getArgs(func) {
+  var args = func.toString().match(/\S+\s*?\(([^)]*)\)/)[1];
+  return args.split(',').map(function(arg) {
+    return arg.replace(/\/\*.*\*\//, '').trim();
+  }).filter(function(arg) {
+    return arg;
+  });
+}
+
+const assert = require('assert');
+const k8s = require('@kubernetes/client-node');
+const fakeConfig = {
+    clusters: [
+        {
+            name: 'cluster',
+            server: 'foo.bar.com',
+        }
+    ],
+    contexts: [
+        {
+            cluster: 'cluster',
+            user: 'user',
+        }
+    ],
+    users: [
+        {
+            name: 'user',
+        }
+    ],
+};
+const kc = new k8s.KubeConfig();
+Object.assign(kc, fakeConfig);
+const k8sApi = kc.makeApiClient(k8s.BatchV1Api);
+
+function testdeleteNamespacedJob() {
+    assert.strictEqual(getArgs(k8sApi.deleteNamespacedJob)[6], 'propagationPolicy')
+}
+
+
+function main() {
+    console.log(getArgs(k8sApi.deleteNamespacedJob))
+    testdeleteNamespacedJob();
+}
+module.exports = main;
+if (!module.parent)
+    main();
