64dc3afaa0d89a026c9c6863983890551b9988d7
jgd5 <root@ip-172-31-35-232.us-west-2.compute.internal>
2019-10-27 19:06:30 +0000

    update Dockerfile.decanlp

--- docker/start.sh
+++ docker/start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -x
-source scl_source enable rh-python36 
+if [ -e /usr/bin/scl_source ]; then
+  source scl_source enable rh-python36 
+fi
 NODE_MAX_OLD_SPACE_SIZE=${NODE_MAX_OLD_SPACE_SIZE:-500}
 node --max_old_space_size=${NODE_MAX_OLD_SPACE_SIZE} /opt/almond-cloud/main.js $*
