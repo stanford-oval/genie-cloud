b1e771ed18696581d3e6442337f77e9267deb779
jgd5 <root@ip-172-31-35-232.us-west-2.compute.internal>
2019-10-27 19:11:42 +0000

    Revert "set max_old_space_size for almond-cloud daemon"
    This reverts commit e0e7e853c4f8949dc6b79274f086996cbe288c0c.
    
    Accidentally committed to master while updating PR #544 .

--- docker/start.sh
+++ docker/start.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-set -x
 source scl_source enable rh-python36 
-NODE_MAX_OLD_SPACE_SIZE=${NODE_MAX_OLD_SPACE_SIZE:-500}
-node --max_old_space_size=${NODE_MAX_OLD_SPACE_SIZE} /opt/almond-cloud/main.js $*
+/opt/almond-cloud/main.js $*
