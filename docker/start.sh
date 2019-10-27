e0e7e853c4f8949dc6b79274f086996cbe288c0c
jgd5 <root@ip-172-31-35-232.us-west-2.compute.internal>
2019-10-27 04:31:27 +0000

    set max_old_space_size for almond-cloud daemon

--- docker/start.sh
+++ docker/start.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+set -x
 source scl_source enable rh-python36 
-/opt/almond-cloud/main.js $*
+NODE_MAX_OLD_SPACE_SIZE=${NODE_MAX_OLD_SPACE_SIZE:-500}
+node --max_old_space_size=${NODE_MAX_OLD_SPACE_SIZE} /opt/almond-cloud/main.js $*
