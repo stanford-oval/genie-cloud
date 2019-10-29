65b6d8c635a4c123c6c2990b50c95604c9b551ab
jgd5 <jim.deng@gmail.com>
2019-10-28 17:35:22 -0700

    update Dockerfile.decanlp (#544)

--- docker/start.sh
+++ docker/start.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-source scl_source enable rh-python36 
-/opt/almond-cloud/main.js $*
+set -x
+if [ -e /usr/bin/scl_source ]; then
+  source scl_source enable rh-python36 
+fi
+NODE_MAX_OLD_SPACE_SIZE=${NODE_MAX_OLD_SPACE_SIZE:-500}
+node --max_old_space_size=${NODE_MAX_OLD_SPACE_SIZE} /opt/almond-cloud/main.js $*
