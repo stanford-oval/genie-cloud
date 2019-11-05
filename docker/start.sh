cb8343d99e8bdd880b16ad41c630433d17efe581
jgd5 <jim.deng@gmail.com>
2019-11-04 23:38:15 -0800

    upgrade dockefiles to ubi8 (#560)

--- docker/start.sh
+++ docker/start.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 set -x
-if [ -e /usr/bin/scl_source ]; then
-  source scl_source enable rh-python36 
-fi
 NODE_MAX_OLD_SPACE_SIZE=${NODE_MAX_OLD_SPACE_SIZE:-500}
 node --max_old_space_size=${NODE_MAX_OLD_SPACE_SIZE} /opt/almond-cloud/main.js $*
