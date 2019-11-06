7526296f33b681dab555b239b54f5f60517842c2
Giovanni Campagna <gcampagn@cs.stanford.edu>
2019-11-05 16:24:29 -0800

    Fix Alexa test
    We don't show the welcome if showWelcome is false

--- tests/website/test_alexa.js
+++ tests/website/test_alexa.js
@@ -65,7 +65,7 @@ async function testAlexa(accessToken) {
     assert.deepStrictEqual(response0, {
        response: {
          outputSpeech: {
-           text: 'Hello! I\'m Almond, your virtual assistant.\nI am part of a research project of Stanford University. I am capable of understanding actions and events over web services.\nPlease keep in mind: I do not chat, and I do not understand questions very well. Please check out the Thingpedia to find out what I understand, or type ‘help’.\nTo start, how about you try one of these examples:\nHi!\n',
+           text: 'Hi!\n',
            type: 'PlainText'
          },
          shouldEndSession: true
