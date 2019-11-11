ad3ab0ad51df57cb40c0f843339e5be9049a3c74
Silei Xu <ray.slxu@gmail.com>
2019-11-11 14:46:13 -0800

    Show a message after successfully submitting a command

--- browser/deps/new-command.js
+++ browser/deps/new-command.js
@@ -30,11 +30,7 @@ module.exports = class ThingTalkTrainer {
         this.thingpedia = new ThingpediaClient(this._developerKey, this._locale);
         this._schemaRetriever = new SchemaRetriever(this.thingpedia);
 
-        this._predicted = false;
-        this._confirmed = false;
-        this._raw = null;
-        this._code = null;
-        this._entities = null;
+        this._init();
 
         $('#input-command-utterance').change(() => {
             this._predicted = false;
@@ -52,6 +48,34 @@ module.exports = class ThingTalkTrainer {
             this._rejectAll(event);
         });
         $('#form-new-command').submit(this._submit.bind(this));
+    }
+
+    _init() {
+        $('#input-command-utterance').val('');
+        $('#input-command-thingtalk').val('');
+        $('#input-command-confirmation').val('');
+        $('#thingtalk-error').text('');
+        $('#thingtalk-group').removeClass('has-error');
+
+        $('#utterance-group').removeClass('hidden');
+        $('#submit').removeClass('hidden');
+
+        $('#results-container').addClass('hidden');
+        $('#results-fail').addClass('hidden');
+        $('#success').hide();
+
+        this._predicted = false;
+        this._confirmed = false;
+        this._raw = null;
+        this._code = null;
+        this._entities = null;
+    }
+
+    _hideAll() {
+        $('#utterance-group').addClass('hidden');
+        $('#thingtalk-group').addClass('hidden');
+        $('#confirmation-group').addClass('hidden');
+        $('#submit').addClass('hidden');
     }
 
     // put accepted thingtalk code into editor
@@ -207,6 +231,9 @@ module.exports = class ThingTalkTrainer {
                 }).catch((e) => {
                     $('#thingtalk-group').addClass('has-error');
                     $('#thingtalk-error').text(this._formatError(e));
+                }).finally(() => {
+                    this._hideAll();
+                    $('#success').show();
                 });
             }
         }
