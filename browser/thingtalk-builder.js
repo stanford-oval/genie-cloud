29aa382222a3f70845c54b8ed0d069f546f1ebce
Silei Xu <ray.slxu@gmail.com>
2019-12-01 17:54:02 -0800

    wip: Add GUI to add input parameter

--- browser/thingtalk-builder.js
+++ browser/thingtalk-builder.js
@@ -18,6 +18,26 @@ const Ast = ThingTalk.Ast;
 const SchemaRetriever = ThingTalk.SchemaRetriever;
 const ThingpediaClient = require('./deps/thingpediaclient');
 
+function prettyprintExample(e, full=true) {
+    const code = ThingTalk.NNSyntax.fromNN(e.target.code, e.target.entities);
+    if (full)
+        return code.prettyprint();
+    if (e.type === 'stream')
+        return code.prettyprint().split('=>')[0];
+    if (e.type === 'query' || e.type === 'action')
+        return code.prettyprint().split('=>')[1];
+    return code;
+}
+
+function fromString(type, value) {
+    if (type.isString)
+        return Ast.Value.String(value);
+    if (type.isNumber && !isNaN(value))
+        return Ast.Value.Number(parseInt(value));
+    throw new Error(`Cannot convert ${value} into ${type.toString()}`);
+
+}
+
 class ThingTalkBuilder {
     constructor() {
         this._locale = document.body.dataset.locale || 'en-US';
@@ -43,6 +63,9 @@ class ThingTalkBuilder {
         this._exampleDiv = $('#example-result');
         this._exampleCandidates = $('#example-candidates');
         this._exampleListHint = $('#example-list-hint');
+
+        // the <div> containing input params of a chosen function
+        this._inputParamsCandidates = $('#thingtalk-add-input-edit');
 
         // the thingtalk output
         this._thingtalkOutput = $('#thingtalk-output');
@@ -88,16 +111,33 @@ class ThingTalkBuilder {
         }
     }
 
+    showInputParams() {
+        this._resetInputParamCandidates();
+
+        let ex;
+        if (this._currentType === 'stream')
+            ex = this._stream;
+        else if (this._currentType === 'query')
+            ex = this._query;
+        else if (this._currentType === 'action')
+            ex = this._action;
+        else
+            throw new Error('Unexpected type');
+
+        for (let slot in ex.target.slotTypes)
+            this._addInputCandidate(slot, ex.target.slotTypes[slot]);
+    }
+
     _updateThingTalk(e) {
         if (this._currentType === 'stream') {
             this._stream = e;
-            $('#thingtalk-when').val(e.target.code.split('=>')[0]);
+            $('#thingtalk-when').val(prettyprintExample(e, false));
         } else if (this._currentType === 'query') {
             this._query = e;
-            $('#thingtalk-get').val(e.target.code.split('=>')[1]);
+            $('#thingtalk-get').val(prettyprintExample(e, false));
         } else if (this._currentType === 'action') {
             this._action = e;
-            $('#thingtalk-do').val(e.target.code.split('=>')[1]);
+            $('#thingtalk-do').val(prettyprintExample(e, false));
         } else {
             throw new Error('Unexpected type');
         }
@@ -106,18 +146,77 @@ class ThingTalkBuilder {
         $('#thingtalk-select').modal('toggle');
     }
 
+
+    _addInputCandidate(name) {
+        let row = $('<div>').addClass('row');
+        let nameDiv = $('<div>').addClass('col-lg-4');
+        nameDiv.append($('<p>').addClass('form-control').text(name));
+
+        let opDiv = $('<div>').addClass('col-lg-3');
+        opDiv.append($('<p>').addClass('form-control').text('='));
+
+        let valueDiv = $('<div>').addClass('col-lg-4');
+        valueDiv.append($('<input>').addClass('form-control').attr('id', `thingtalk-input-value-${name}`));
+
+        row.append(nameDiv);
+        row.append(opDiv);
+        row.append(valueDiv);
+
+        this._inputParamsCandidates.append(row);
+    }
+
+    updateInput() {
+        if (this._currentType === 'stream') {
+            let entities = {};
+            for (let i = 0; i < this._stream.target.slots.length; i++) {
+                let name = this._stream.target.slots[i];
+                let value = $(`#thingtalk-input-value-${name}`).val();
+                if (value)
+                    entities[`SLOT_${i}`] = fromString(this._stream.target.slotTypes[name], value);
+            }
+            this._stream.target.entities = entities;
+            let stream = ThingTalk.NNSyntax.fromNN(this._stream.target.code, entities).prettyprint();
+            $('#thingtalk-when').val(stream.split('=>')[0]);
+        } else if (this._currentType === 'query') {
+            let entities = {};
+            for (let i = 0; i < this._query.target.slots.length; i++) {
+                let name = this._query.target.slots[i];
+                let value = $(`#thingtalk-input-value-${name}`).val();
+                if (value)
+                    entities[`SLOT_${i}`] = fromString(this._query.target.slotTypes[name], value);
+            }
+            this._query.target.entities = entities;
+            let query = ThingTalk.NNSyntax.fromNN(this._query.target.code, entities).prettyprint();
+            $('#thingtalk-get').val(query.split('=>')[1]);
+        } else if (this._currentType === 'action') {
+            let entities = {};
+            for (let i = 0; i < this._action.target.slots.length; i++) {
+                let name = this._action.target.slots[i];
+                let value = $(`#thingtalk-input-value-${name}`).val();
+                if (value)
+                    entities[`SLOT_${i}`] = fromString(this._action.target.slotTypes[name], value);
+            }
+            this._action.target.entities = entities;
+            let action = ThingTalk.NNSyntax.fromNN(this._action.target.code, entities).prettyprint();
+            $('#thingtalk-do').val(action.split('=>')[1]);
+        } else {
+            throw new Error('Unexpected type');
+        }
+        this._thingtalkOutput.val(this._prettyprint());
+    }
+
     _prettyprint() {
         let rule, stream, query, action;
         if (!this._stream && !this._query && !this._action)
             return 'Please choose at least one function.';
         if (this._action)
-            action = ThingTalk.Grammar.parse(this._action.target.code).rules[0].actions[0];
+            action = ThingTalk.NNSyntax.fromNN(this._action.target.code, this._action.target.entities).rules[0].actions[0];
         else
             action = ThingTalk.Generate.notifyAction();
         if (this._stream)
-            stream = ThingTalk.Grammar.parse(this._stream.target.code).rules[0].stream;
+            stream = ThingTalk.NNSyntax.fromNN(this._stream.target.code, this._stream.target.entities).rules[0].stream;
         if (this._query)
-            query = ThingTalk.Grammar.parse(this._query.target.code).rules[0].table;
+            query = ThingTalk.NNSyntax.fromNN(this._query.target.code, this._query.target.entities).rules[0].table;
         if (stream && query) {
             rule = new Ast.Statement.Rule(
                 new Ast.Stream.Join(stream, query, [], null), [action]
@@ -153,6 +252,10 @@ class ThingTalkBuilder {
         this._exampleCandidates.empty();
     }
 
+    _resetInputParamCandidates() {
+        this._inputParamsCandidates.empty();
+    }
+
 
     async loadExamples(dataset, maxCount) {
         const parsed = await ThingTalk.Grammar.parseAndTypecheck(dataset, this._schemaRetriever);
@@ -185,11 +288,11 @@ class ThingTalkBuilder {
         let slots = [];
         let slotTypes = {};
         for (let name in ex.args) {
-            slotTypes[name] = String(ex.args[name]);
+            slotTypes[name] = ex.args[name];
             slots.push(name);
         }
 
-        let code = newprogram.prettyprint();
+        let code = ThingTalk.NNSyntax.toNN(newprogram, {});
         let monitorable;
         if (ex.type === 'stream')
             monitorable = true;
@@ -226,9 +329,32 @@ $(() => {
         $('#thingtalk-select').modal('show');
     });
 
+    $('#thingtalk-when-add-input').click(() => {
+        builder.reset('stream');
+        $('#thingtalk-add-input').modal('show');
+    });
+    $('#thingtalk-get-add-input').click(() => {
+        builder.reset('query');
+        $('#thingtalk-add-input').modal('show');
+    });
+    $('#thingtalk-do-add-input').click(() => {
+        builder.reset('action');
+        $('#thingtalk-add-input').modal('show');
+    });
+
+
     $('#thingtalk-search-device').click(async () => {
         const key = $('#thingtalk-search-device-input').val();
         const devices = await builder.searchDevice(key);
         builder.showDevices(devices.data);
+    });
+
+    $('#thingtalk-add-input').on('shown.bs.modal', () => {
+        builder.showInputParams();
+    });
+
+    $('#thingtalk-add-input-submit').click(() => {
+        builder.updateInput();
+        $('#thingtalk-add-input').modal('toggle');
     });
 });
