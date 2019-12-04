38e24e2541c6bb12a59c57afa12f4468e6ec56be
Euirim Choi <euirim@me.com>
2019-12-04 00:29:30 -0800

    Fix extraneous import

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -3,16 +3,21 @@ import Link from 'next/link';
 
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import './Navbar.scss';
 
 export default props => (
   <Navbar bg="light" expand="lg" fixed="top">
-    <Navbar.Brand><Link href="/"><a className="nav-brand-link">Almond</a></Link></Navbar.Brand>
+    <Navbar.Brand>
+      <Link href="/">
+        <a className="nav-brand-link">Almond</a>
+      </Link>
+    </Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="mr-auto">
-        <Link href="/developers/train" as={Nav.Link}><a className="nav-local-link">Train</a></Link>
+        <Link href="/developers/train">
+          <a className="nav-local-link">Train</a>
+        </Link>
       </Nav>
     </Navbar.Collapse>
   </Navbar>
