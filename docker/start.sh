ed256f6326fb8f147530d818d9c95ed49345c603
Giovanni Campagna <gcampagn@cs.stanford.edu>
2019-12-14 17:47:57 -0800

    Coalesce dockerfiles
    Remove the distinction of decanlp vs non-decanlp images, and always
    base the images on decanlp. This simplifies the build, and is ok
    now that the decanlp images are smaller because they do not have
    the embeddings. Also free up some space by removing unused packages
    after the build

--- docker/start.sh
+++ docker/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -x
 NODE_MAX_OLD_SPACE_SIZE=${NODE_MAX_OLD_SPACE_SIZE:-500}
-node --max_old_space_size=${NODE_MAX_OLD_SPACE_SIZE} /opt/almond-cloud/main.js $*
+node --max_old_space_size=${NODE_MAX_OLD_SPACE_SIZE} /opt/almond-cloud/main.js "$@"
