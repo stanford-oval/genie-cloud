c8a18144d29f323fd835b52eb0440bdd48bb6b29
Euirim Choi <euirim@me.com>
2020-01-09 19:39:00 -0800

    Finish preliminary revamp

---
+++ public/javascripts/get-involved.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var glide = new Glide('#jumbotron-carousel', {
+  type: 'carousel',
+  perView: 5,
+  focusAt: 'center',
+  autoplay: 2500,
+  animationDuration: 1500,
+  breakpoints: {
+    1800: {
+      perView: 4
+    },
+    1450: {
+      perView: 3
+    },
+    1100: {
+      perView: 2
+    },
+    780: {
+      perView: 1
+    }
+  }
+});
+
+glide.mount();
