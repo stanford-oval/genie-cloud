64a9aab2a34d1ef690bbdfefab3ade3e162e046b
Euirim Choi <euirim@me.com>
2020-01-10 15:31:32 -0800

    Use scripts block

--- public/javascripts/get-involved.js
+++ public/javascripts/get-involved.js
@@ -1,25 +1,26 @@
 'use strict';
-
-var glide = new Glide('#jumbotron-carousel', {
-  type: 'carousel',
-  perView: 5,
-  focusAt: 'center',
-  autoplay: 2500,
-  animationDuration: 1500,
-  breakpoints: {
-    1800: {
-      perView: 4
-    },
-    1450: {
-      perView: 3
-    },
-    1100: {
-      perView: 2
-    },
-    780: {
-      perView: 1
+$(function() {
+  var glide = new Glide('#jumbotron-carousel', {
+    type: 'carousel',
+    perView: 5,
+    focusAt: 'center',
+    autoplay: 2500,
+    animationDuration: 1500,
+    breakpoints: {
+      1800: {
+        perView: 4
+      },
+      1450: {
+        perView: 3
+      },
+      1100: {
+        perView: 2
+      },
+      780: {
+        perView: 1
+      }
     }
-  }
-});
+  });
 
-glide.mount();
+  glide.mount();
+});
