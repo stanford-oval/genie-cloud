00ac9bd2d2fc7f7af86695e139ad3cc987da67d7
Euirim Choi <euirim@me.com>
2020-01-09 19:48:30 -0800

    Delete unnecessary frontend components

--- frontend/src/components/BodyColumn.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import './BodyColumn.scss';
-
-export default props => (
-  <div className="body-column">
-    {props.children}
-  </div>
-);
--- frontend/src/config.js
+++
@@ -1,5 +0,0 @@
-export default {
-  env: process.env.NODE_ENV,
-  mode: process.env.MODE,
-  githubApiEndpoint: process.env.GITHUB_API_ENDPOINT,
-};
