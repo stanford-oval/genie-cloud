d1a808da9c13fa241c82d51415e885796035c1fe
Giovanni Campagna <gcampagn@cs.stanford.edu>
2020-03-24 19:22:13 -0700

    Use genienlp 0.2.0a2
    Less broken

--- tests/install-nlp-deps.sh
+++ tests/install-nlp-deps.sh
@@ -7,7 +7,7 @@ set -o pipefail
 srcdir=`dirname $0`/..
 srcdir=`realpath $srcdir`
 
-which genienlp >/dev/null 2>&1 || pip3 install --user 'genienlp==0.2.0a1'
+which genienlp >/dev/null 2>&1 || pip3 install --user 'genienlp==0.2.0a2'
 which genienlp
 
 mkdir -p $srcdir/tests/embeddings
