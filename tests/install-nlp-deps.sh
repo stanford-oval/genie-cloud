011b8cd314a1291feb9d7d0fa3c129c50836c1c7
Giovanni Campagna <gcampagn@cs.stanford.edu>
2020-04-03 11:56:47 -0700

    Update dependencies

--- tests/install-nlp-deps.sh
+++ tests/install-nlp-deps.sh
@@ -7,7 +7,7 @@ set -o pipefail
 srcdir=`dirname $0`/..
 srcdir=`realpath $srcdir`
 
-which genienlp >/dev/null 2>&1 || pip3 install --user 'genienlp==0.2.0b2'
+which genienlp >/dev/null 2>&1 || pip3 install --user 'genienlp==0.2.0'
 which genienlp
 
 mkdir -p $srcdir/tests/embeddings
