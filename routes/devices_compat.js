efb60060b027f741434bad21fa2b550dcf3eb4b5
Ryan Cheng <ryachen@nuevaschool.org>
2020-06-20 18:00:30 -0700

    added new api endpoint to proxy oauth requests

--- routes/devices_compat.js
+++ routes/devices_compat.js
@@ -26,20 +26,25 @@ const EngineManager = require('../almond/enginemanagerclient');
 
 var router = express.Router();
 
-router.use(user.requireLogIn);
-
 router.get('/oauth2/callback/:kind', (req, res, next) => {
-    const kind = req.params.kind;
 
-    EngineManager.get().getEngine(req.user.id).then(async (engine) => {
+    if (!req.session.redirect) {
+      router.use(user.requireLogIn);
+      const kind = req.params.kind;
+
+      EngineManager.get().getEngine(req.user.id).then(async (engine) => {
         await engine.completeOAuth(kind, req.url, req.session);
         if (req.session['device-redirect-to']) {
-            res.redirect(303, req.session['device-redirect-to']);
-            delete req.session['device-redirect-to'];
+          res.redirect(303, req.session['device-redirect-to']);
+          delete req.session['device-redirect-to'];
         } else {
-            res.redirect(303, '/me');
+           res.redirect(303, '/me');
         }
-    }).catch(next);
+      }).catch(next);
+    }else{
+      const server_redirect = req.session.redirect + req.url;
+      res.redirect(303, server_redirect);
+    }
 });
 
 module.exports = router;
