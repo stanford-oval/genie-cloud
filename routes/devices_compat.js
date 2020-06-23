31e6cf7c02282eb5d4e8ddc7662a915581b7353a
Ryan Cheng <ryachen@nuevaschool.org>
2020-06-23 14:04:20 -0700

    reworked confirmation page

--- routes/devices_compat.js
+++ routes/devices_compat.js
@@ -27,23 +27,22 @@ const EngineManager = require('../almond/enginemanagerclient');
 var router = express.Router();
 
 router.get('/oauth2/callback/:kind', (req, res, next) => {
-
     if (!req.session.redirect) {
-      router.use(user.requireLogIn);
-      const kind = req.params.kind;
+        router.use(user.requireLogIn);
+        const kind = req.params.kind;
 
-      EngineManager.get().getEngine(req.user.id).then(async (engine) => {
-        await engine.completeOAuth(kind, req.url, req.session);
-        if (req.session['device-redirect-to']) {
-            res.redirect(303, req.session['device-redirect-to']);
-            delete req.session['device-redirect-to'];
-        } else {
-            res.redirect(303, '/me');
-        }
-    }).catch(next);
-    }else{
-      const server_redirect = req.session.redirect + req.url;
-      res.redirect(303, server_redirect);
+        EngineManager.get().getEngine(req.user.id).then(async (engine) => {
+            await engine.completeOAuth(kind, req.url, req.session);
+            if (req.session['device-redirect-to']) {
+                res.redirect(303, req.session['device-redirect-to']);
+                delete req.session['device-redirect-to'];
+            } else {
+                res.redirect(303, '/me');
+            }
+        }).catch(next);
+    } else {
+        const server_redirect = req.session.redirect + "/devices" + req.url;
+        res.redirect(303, server_redirect);
     }
 });
 
