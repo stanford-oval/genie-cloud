79d2c847e472be502cd5f58403d7bf0378c63130
Giovanni Campagna <gcampagn@cs.stanford.edu>
2020-07-27 10:24:21 -0700

    Update genienlp (#785)
    We need the genienlp library to be the same version as the trained
    model, otherwise it crashes with an obscure error.

--- tests/install-nlp-deps.sh
+++ tests/install-nlp-deps.sh
@@ -7,7 +7,7 @@ set -o pipefail
 srcdir=`dirname $0`/..
 srcdir=`realpath $srcdir`
 
-which genienlp >/dev/null 2>&1 || pip3 install --user 'genienlp==0.3.0'
+which genienlp >/dev/null 2>&1 || pip3 install --user 'git+https://github.com/stanford-oval/genienlp@8cbfe50e0e92b97e68dc013a3c48788df23c2f7a#egg=genienlp'
 which genienlp
 
 mkdir -p $srcdir/tests/embeddings
