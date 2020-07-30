d3a00812569bf0234dd7c41f77a8f1de406d7fdf
Giovanni Campagna <gcampagn@cs.stanford.edu>
2020-07-30 16:13:37 -0700

    Relicense to Apache 2.0 (#786)
    Like the rest of the Genie platform

--- training/workers/generate-contextual-worker.js
+++ training/workers/generate-contextual-worker.js
@@ -1,12 +1,22 @@
 // -*- mode: js; indent-tabs-mode: nil; js-basic-offset: 4 -*-
 //
-// This file is part of Genie
+// This file is part of Almond
 //
 // Copyright 2019 The Board of Trustees of the Leland Stanford Junior University
 //
-// Author: Giovanni Campagna <gcampagn@cs.stanford.edu>
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-// See COPYING for details
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// Author: Giovanni Campagna <gcampagn@cs.stanford.edu>
 "use strict";
 
 const Tp = require('thingpedia');
