d626af06d9aa1540f24cb2903bb7f6ad6df21273
Giovanni Campagna <gcampagn@cs.stanford.edu>
2020-08-10 16:00:05 -0700

    Update genienlp again
    Fix a version incompatibility

--- tests/install-nlp-deps.sh
+++ tests/install-nlp-deps.sh
@@ -7,7 +7,7 @@ set -o pipefail
 srcdir=`dirname $0`/..
 srcdir=`realpath $srcdir`
 
-which genienlp >/dev/null 2>&1 || pip3 install --user 'git+https://github.com/stanford-oval/genienlp@74915155fa683fed4345674c3e4d7df79cc98580#egg=genienlp'
+which genienlp >/dev/null 2>&1 || pip3 install --user 'git+https://github.com/stanford-oval/genienlp@5139c716558d59a4b05af9a2c27fa9569bf92d1d#egg=genienlp'
 which genienlp
 
 mkdir -p $srcdir/tests/embeddings
