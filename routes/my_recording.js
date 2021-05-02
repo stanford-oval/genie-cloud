34aef5521fc8bf89773f38b45b9af4ff894b7388
Giovanni Campagna <gcampagn@cs.stanford.edu>
2021-05-01 17:45:12 -0700

    Handle invalid conversation IDs correctly
    These can occur in case the conversation is recycled after timeout,
    or in case the engine was restarted.

--- routes/my_recording.js
+++ routes/my_recording.js
@@ -162,10 +162,12 @@ router.get('/log/:id', (req, res, next) => {
         return engine.getConversation(req.params.id);
     }).then(async (conversation) => {
         if (!conversation) {
+            res.status(404);
             res.json({ status : 'not found' });
         } else {
             const log = await conversation.log();
             if (!log) {
+                res.status(404);
                 res.json({ status: 'not found' });
             } else {
                 res.json({
