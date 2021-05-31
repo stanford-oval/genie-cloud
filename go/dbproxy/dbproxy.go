5e7ae1cac2535acb857fc8d0e693e894f5654b50
Jim Deng <jim.deng@gmail.com>
2021-05-31 19:36:23 +0000

    add commandline parsing and main package

--- go/dbproxy/dbproxy.go
+++ go/dbproxy/dbproxy.go
@@ -11,15 +11,31 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-package main
+package dbproxy
 
 import (
 	"almond-cloud/sql"
+	"flag"
+	"fmt"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
-func main() {
+var (
+	flagSet = flag.NewFlagSet("dbproxy", flag.ExitOnError)
+	port    = flagSet.Int("port", 8888, "port")
+)
+
+func Usage() {
+	fmt.Printf("Usage of %s dbproxy", os.Args[0])
+	flagSet.PrintDefaults()
+}
+
+func Run(args []string) {
+
+	flagSet.Parse(args)
+	fmt.Printf("--- port:%v", *port)
 	dsn := "newuser:password@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
 	sql.InitMySQL(dsn)
 	r := gin.Default()
@@ -40,5 +56,5 @@ func main() {
 	r.POST("/synctable/:name/:userid", syncTableInsertOne)
 	r.DELETE("/synctable/:name/:userid/:uniqueid/:millis", syncTableDeleteIfRecent)
 	r.DELETE("/synctable/:name/:userid/:uniqueid", syncTableDeleteOne)
-	r.Run() // listen and serve on 0.0.0.0:8080
+	r.Run(fmt.Sprintf("0.0.0.0:%d", *port))
 }
