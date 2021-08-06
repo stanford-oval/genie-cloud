f12dc09a3b0c30a9047e2895734295d21d528a90
nrser <neil@neilsouza.com>
2021-08-05 21:54:38 -0700

    [WIP] Start port of staging k8s setup for local dev use
    In particular, `@/dev/k8s/config/config.yaml` has not been adapted; want
    to start with the `staging` values so I have a record of what needed to
    be changed.

---
+++ dev/k8s/bin/almond-dev.create.bash
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Common / useful `set` commands
+set -Ee # Exit on error
+set -o pipefail # Check status of piped commands
+set -u # Error on undefined vars
+# set -v # Print everything
+# set -x # Print commands (with expanded vars)
+
+REPO_ROOT="$(git rev-parse --show-toplevel)"
+
+cd "${REPO_ROOT}/dev/k8s" && \
+	kustomize build | kubectl apply -f -
