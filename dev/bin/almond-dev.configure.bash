f233dffc74a88c571e5244e63ee6262617f463bd
nrser <neil@neilsouza.com>
2021-08-30 15:12:27 -0700

    Have config script generate .local.yaml files

--- dev/bin/almond-dev.configure.bash
+++ dev/bin/almond-dev.configure.bash
@@ -8,19 +8,63 @@ set -u # Error on undefined vars
 # set -x # Print commands (with expanded vars)
 
 REPO_ROOT="$(git rev-parse --show-toplevel)"
-DEST_PATH="${REPO_ROOT}/k8s/config/dev/secret.yaml"
-
 source "${REPO_ROOT}/dev/.env"
+
+SECRET_PATH="${REPO_ROOT}/k8s/config/dev/secret.yaml"
+BOOSTRAP_JOB_PATH="${REPO_ROOT}/k8s/bootstrap/dev/job.local.yaml"
+FRONTEND_DEPLOYMENT_PATH="${REPO_ROOT}/k8s/frontend/dev/deployment.local.yaml"
+
+SHARED_PATH="${REPO_ROOT}/tmp/shared"
+
+
+for x in devices icons backgrounds blog-assets template-files/en; do
+	mkdir -p "${SHARED_PATH}/download/$x"
+done
 
 SECRET_KEY="$(openssl rand -hex 32)"
 AES_SECRET_KEY="$(openssl rand -hex 16)"
 JWT_SIGNING_KEY="$(openssl rand -hex 32)"
 
-cat <<END >"${DEST_PATH}"
+cat <<END >"${SECRET_PATH}"
 MAILGUN_USER: ${MAILGUN_SMTP_USERNAME}
 MAILGUN_PASSWORD: ${MAILGUN_SMTP_PASSWORD}
 EMAIL_TO_ADMIN: ${DEVELOPER_EMAIL}
 SECRET_KEY: ${SECRET_KEY}
 AES_SECRET_KEY: ${AES_SECRET_KEY}
 JWT_SIGNING_KEY: ${JWT_SIGNING_KEY}
+END
+
+cat <<END >"${BOOSTRAP_JOB_PATH}"
+apiVersion: batch/v1
+kind: Job
+metadata:
+  name: bootstrap
+spec:
+  template:
+    spec:
+      volumes:
+        - name: shared
+          hostPath:
+            path: ${SHARED_PATH}
+END
+
+cat <<END >"${FRONTEND_DEPLOYMENT_PATH}"
+---
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: frontend
+spec:
+  template:
+    spec:
+      volumes:
+        - name: src
+          hostPath:
+            path: ${REPO_ROOT}/src
+        - name: views
+          hostPath:
+            path: ${REPO_ROOT}/views
+        - name: shared
+          hostPath:
+            path: ${SHARED_PATH}
 END
