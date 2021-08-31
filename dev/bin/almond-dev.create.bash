ffd2c9d14b853c082b7eba3ac5528a2ad184d035
nrser <neil@neilsouza.com>
2021-08-31 10:29:21 -0700

    Remove hard-coded kubectl context
    and check it's not research|serving|prod

--- dev/bin/almond-dev.create.bash
+++ dev/bin/almond-dev.create.bash
@@ -7,7 +7,13 @@ set -u # Error on undefined vars
 # set -v # Print everything
 # set -x # Print commands (with expanded vars)
 
+context="$(kubectl config current-context)"
+if echo "${context}" | grep -q 'research|serving|prod' ; then
+   echo "ERROR not a local dev context: ${context}"
+	 exit 1
+fi
+
 REPO_ROOT="$(git rev-parse --show-toplevel)"
 
 cd "${REPO_ROOT}/k8s" && \
-	kustomize build dev | kubectl --context docker-desktop apply -f -
+	kustomize build dev | kubectl apply -f -
