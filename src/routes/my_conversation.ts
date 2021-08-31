da494d5398a6014cc5e29a09c2783fb0d9393374
Giovanni Campagna <gcampagn@cs.stanford.edu>
2021-08-31 15:01:12 -0700

    conversation: add a flag to skip replaying the history
    For voice devices, keeping track of the message ID is problematic.
    Instead, just don't replay the history.

--- src/routes/my_conversation.ts
+++ src/routes/my_conversation.ts
@@ -146,6 +146,7 @@ interface ConversationQueryParams {
     hide_welcome ?: '1'|''|undefined;
     id ?: string;
     flags ?: Record<string, unknown>;
+    skip_history ?: '1'|''|undefined;
 }
 
 async function doConversation(user : userModel.RowWithOrg, anonymous : boolean, ws : WebSocket, query : ConversationQueryParams) {
@@ -167,6 +168,8 @@ async function doConversation(user : userModel.RowWithOrg, anonymous : boolean, 
         }
 
         const options = {
+            replayHistory: !query.skip_history,
+
             showWelcome: !query.hide_welcome,
             anonymous,
             dialogueFlags: flags,
