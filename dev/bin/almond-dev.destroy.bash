4700107e7baf71483ef7f86b4dfa652b0de00c5d
nrser <neil@neilsouza.com>
2021-08-31 10:29:21 -0700

    Remove hard-coded kubectl context
    and check it's not research|serving|prod

--- dev/bin/almond-dev.destroy.bash
+++ dev/bin/almond-dev.destroy.bash
@@ -7,4 +7,10 @@ set -u # Error on undefined vars
 # set -v # Print everything
 # set -x # Print commands (with expanded vars)
 
-kubectl --context kind-kind delete namespace almond-dev
+context="$(kubectl config current-context)"
+if echo "${context}" | grep -q 'research|serving|prod' ; then
+   echo "ERROR not a local dev context: ${context}"
+	 exit 1
+fi
+
+kubectl delete namespace almond-dev
