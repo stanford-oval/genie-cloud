7c5f7a05e458562c9db50d4590b75855fe75ea9d
Giovanni Campagna <gcampagn@cs.stanford.edu>
2021-09-08 08:39:52 -0700

    Update version of genienlp used in tests

--- tests/install-nlp-deps.sh
+++ tests/install-nlp-deps.sh
@@ -8,7 +8,7 @@ srcdir=`dirname $0`/..
 srcdir=`realpath $srcdir`
 
 pip install --user --upgrade pip
-which genienlp >/dev/null 2>&1 || pip3 install --user genienlp==0.6.0
+which genienlp >/dev/null 2>&1 || pip3 install --user genienlp==0.7.0a2
 which genienlp
 
 mkdir -p $srcdir/tests/embeddings
